7e8df5aaf3dc05ee81077c7b8d04fd4ca68af65a
aasimd <dongarkar21@gmail.com>
2023-10-13 15:45:29 +0530

    item card style changed

--- vite-project/src/components/Inventory/ItemCard/ItemCard.jsx
+++ vite-project/src/components/Inventory/ItemCard/ItemCard.jsx
@@ -10,46 +10,43 @@ export const ItemCard = ({ item }) => {
 	const { _id, name, quantity, price, category } = item;
 	return (
 		<div className="item-card">
-			<div className="item-category-pic-container">
-				<ItemCategoryPic category={category} />
-			</div>
 			<div className="item-info">
-				<div className="item-name-category">
-					<div>
-						<b>{name}</b> <br /> {category}
-					</div>
-					<div className="item-edit-delete-buttons">
-						<button
-							title="Edit"
-							onClick={() => {
-								dispatch({ type: "setEditItemId", payload: _id });
-								dispatch({ type: "setShowItemEditModal", payload: true });
-							}}
-						>
-							<FontAwesomeIcon icon={faPenSquare} />
-						</button>
-						<button
-							title="Delete"
-							onClick={() => {
-								dispatch({ type: "setDeleteItemId", payload: _id });
-								dispatch({ type: "setShowItemDeleteModal", payload: true });
-							}}
-						>
-							<FontAwesomeIcon icon={faTrash} />
-						</button>
-					</div>
+				<div className="item-category-pic-container">
+					<ItemCategoryPic category={category} />
 				</div>
-				<div className="item-quantity-price">
-					<div>
-						InStock:
-						<br />
-						<b>{quantity}</b> pcs
-					</div>
-					<div>
-						<b>
-							{price}$ <br /> / piece
-						</b>
-					</div>
+				<div>
+					<b>{name}</b> <br /> {category}
+				</div>
+				<div className="item-edit-delete-buttons">
+					<button
+						title="Edit"
+						onClick={() => {
+							dispatch({ type: "setEditItemId", payload: _id });
+							dispatch({ type: "setShowItemEditModal", payload: true });
+						}}
+					>
+						<FontAwesomeIcon icon={faPenSquare} />
+					</button>
+					<button
+						title="Delete"
+						onClick={() => {
+							dispatch({ type: "setDeleteItemId", payload: _id });
+							dispatch({ type: "setShowItemDeleteModal", payload: true });
+						}}
+					>
+						<FontAwesomeIcon icon={faTrash} />
+					</button>
+					<div className="item-name-category"></div>
+				</div>
+			</div>
+			<div className="item-quantity-price">
+				<div>
+					InStock: <b>{quantity}</b> pcs
+				</div>
+				<div>
+					<b>
+						{price}$ / piece
+					</b>
 				</div>
 			</div>
 		</div>
